65e917f8297478e843fb9d6aa547b65c22485c6e
Darren <darren3981@gmail.com>
2018-11-07 13:11:50 -0600

    added new file to ignore

--- range.py
+++ range.py
@@ -12,11 +12,10 @@ class Range:
     async def range(self, context):
         message = context.message.content
         sname = message[8:]
-        search = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/search/?categories=solar_system&datasource=tranquility&language=en-us&search=' + sname + '&strict=false'))
-        print(search['solar_system'][0])
-        search_name = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/universe/systems/' + str(search['solar_system'][0]) + '/?datasource=tranquility&language=en-us'))
-        system = search_name['name']
-        range_cap = 'http://evemaps.dotlan.net/range/Thanatos,5/' + str(system)
+        search = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/search/?categories=solar_system&datasource=tranquility&language=en-us&search=' + sname + '&strict=false')) #searchs for system name for system id        #print(search['solar_system'][0])
+        search_name = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/universe/systems/' + str(search['solar_system'][0]) + '/?datasource=tranquility&language=en-us')) #pulls proper system name based of system id
+        system = search_name['name'] 
+        range_cap = 'http://evemaps.dotlan.net/range/Thanatos,5/' + str(system) #adds system name to dotlan urls
         range_super = 'http://evemaps.dotlan.net/range/Nyx,5/' + str(system)
         await self.client.say('the range for caps is ' + range_cap)
         await self.client.say('the range for supers is ' + range_super)
