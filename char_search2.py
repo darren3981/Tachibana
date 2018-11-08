cc32c022b5a48be897c84991236937423c3118c5
Darren <darren3981@gmail.com>
2018-11-08 09:47:11 -0600

    added a strict modifier to the command

--- char_search2.py
+++ char_search2.py
@@ -8,12 +8,37 @@ from urllib.parse import quote
 class Char_search:
     def __init__(self,client):
         self.client = client
-    @commands.group(pass_context=True, hidden=True)
-    async def char(self, context):
-        message = context.message.content
-        name = message[7:]
+    @commands.group(pass_context=True, invoke_without_command=True) # invoke_without_command will only run this command if not sub command is invoked
+    async def char(self, ctx):
+        if ctx.invoked_subcommand is None:
+            message = ctx.message.content
+            name = message[7:]
+            url_search = urllib.parse.quote(name)
+            search = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/search/?categories=character&datasource=tranquility&language=en-us&search=' + url_search + '&strict=false'))
+            info_url = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/characters/' + str(search['character'][0]) + '/?datasource=tranquility'))
+            try:
+                Asearch = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/alliances/' + str(info_url['alliance_id']) + '/?datasource=tranquility'))
+            except:
+                Asearch = 'Character is not in an Alliance!'
+            Csearch = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/corporations/' + str(info_url['corporation_id']) + '/?datasource=tranquility'))
+            
+            embed=discord.Embed(title=info_url['name'], color=0xfa14e9)
+            embed.set_thumbnail(url='https://imageserver.eveonline.com/Character/' + str(search['character'][0]) + '_128.jpg')
+            embed.add_field(name='Corporation', value=('Character is in ' + Csearch['name'] + '(' + Csearch['ticker'] + ')'), inline=False)
+            try:
+                embed.add_field(name='Alliance', value=('Character is in ' + Asearch['name'] + '(' + Asearch['ticker'] + ')'), inline=False)
+            except:
+                embed.add_field(name='Alliance', value=(str(Asearch)), inline=False)
+            
+                
+            await self.client.say(embed=embed)
+
+    @char.command(pass_context=True)
+    async def strict(self, ctx):
+        message = ctx.message.content
+        name = message[14:]
         url_search = urllib.parse.quote(name)
-        search = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/search/?categories=character&datasource=tranquility&language=en-us&search=' + url_search + '&strict=false'))
+        search = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/search/?categories=character&datasource=tranquility&language=en-us&search=' + url_search + '&strict=true'))
         info_url = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/characters/' + str(search['character'][0]) + '/?datasource=tranquility'))
         try:
             Asearch = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/alliances/' + str(info_url['alliance_id']) + '/?datasource=tranquility'))
