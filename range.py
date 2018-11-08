da2984e0172d40fa721f077f80d2d9588aa7f458
Darren <darren3981@gmail.com>
2018-11-08 09:55:07 -0600

    added embed and ranges for blops and jf

--- range.py
+++ range.py
@@ -17,8 +17,14 @@ class Range:
         system = search_name['name'] 
         range_cap = 'http://evemaps.dotlan.net/range/Thanatos,5/' + str(system) #adds system name to dotlan urls
         range_super = 'http://evemaps.dotlan.net/range/Nyx,5/' + str(system)
-        await self.client.say('the range for caps is ' + range_cap)
-        await self.client.say('the range for supers is ' + range_super)
+        range_blops = 'http://evemaps.dotlan.net/range/Panther,5/' + str(system)
+        range_jf = 'http://evemaps.dotlan.net/range/Ark,5/' + str(system)
+        embed=discord.Embed(title="Ranges", description="Jump ranges from given system", color=0xfa14e9)
+        embed.add_field(name='Capital', value= range_cap, inline=False)
+        embed.add_field(name='Supers', value=range_super, inline=False)
+        embed.add_field(name='Blops', value=range_blops, inline=False)
+        embed.add_field(name='JF', value=range_jf, inline=False)
+        await self.client.say(embed=embed)
 
 def setup(client):
     client.add_cog(Range(client))   #adds the cog to the bot
