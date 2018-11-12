52267cee52a8748949aec7d9d40de76202625db5
Darren <darren3981@gmail.com>
2018-11-12 13:28:38 -0600

    added exception handling for bad search

--- cogs/price.py
+++ cogs/price.py
@@ -14,13 +14,18 @@ class Price:
             message = ctx.message.content
             name = message[8:]
             url_search = urllib.parse.quote(name)
-            search = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/search/?categories=inventory_type&datasource=tranquility&language=en-us&search=' + str(url_search) + '&strict=true'))
-            info_url = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/markets/10000002/orders/?datasource=tranquility&order_type=sell&page=1&type_id=' + str(search['inventory_type'][0])))
-            output_list = [x['price'] for x in info_url]
-            min_price = min(output_list)
-            embed=discord.Embed(title= name +' Price', color=0xfa14e9)
-            embed.set_thumbnail(url='https://imageserver.eveonline.com/Type/' + str(search['inventory_type'][0]) + '_64.png')
-            embed.add_field(name='current price' , value=str(('{:,}'.format(min_price)) ) + ' isk', inline=False)
+            try:
+                search = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/search/?categories=inventory_type&datasource=tranquility&language=en-us&search=' + str(url_search) + '&strict=true'))
+                info_url = json.load(urllib.request.urlopen('https://esi.evetech.net/latest/markets/10000002/orders/?datasource=tranquility&order_type=sell&page=1&type_id=' + str(search['inventory_type'][0])))
+                output_list = [x['price'] for x in info_url]
+                min_price = min(output_list)
+                embed=discord.Embed(title= name +' Price', color=0xfa14e9)
+                embed.set_thumbnail(url='https://imageserver.eveonline.com/Type/' + str(search['inventory_type'][0]) + '_64.png')
+                embed.add_field(name='current price' , value=str(('{:,}'.format(min_price)) ) + ' isk', inline=False)
+            except:
+                embed=discord.Embed(title= 'lol learn to spell please', color=0xfa14e9)
+           
+           
             await self.client.say(embed=embed)
             
 
