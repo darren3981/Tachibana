28928616f3a936130679dff693f1ea346ae29be8
Darren <darren3981@gmail.com>
2018-12-05 16:50:10 -0600

    removed extra comments

--- cogs/Ping.py
+++ cogs/Ping.py
@@ -4,10 +4,7 @@ from discord.ext import commands
 class Ping:
     def __init__(self,client):
         self.client = client
-
-    #async def on_message_delete(self, message):
-        #await self.client.send_message(message.channel, 'Message deleted.')
-
+        
     @commands.command(pass_context=True)
     async def ping(self, context):
         await self.client.say(context.message.author.mention + " Pong! :ping_pong:")
